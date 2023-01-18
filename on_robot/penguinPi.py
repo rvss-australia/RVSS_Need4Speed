8026c7bf58327f8f1a73faa124663e8c274295b1
Dimity Miller <miller24@QUT-PA00154618.qut.edu.au>
2023-01-19 09:52:51 +1000

    test script

---
+++ on_robot/penguinPi.py
@@ -0,0 +1,26 @@
+import numpy as np
+import requests
+import cv2      
+from picamera import PiCamera
+import time
+
+def set_velocity(vel0, vel1):
+    r = requests.get("http://localhost:8080/robot/set/velocity?value="+str(vel0)+","+str(vel1))
+
+
+class CameraPiBot():
+    def __init__(self, framerate = 30, resolution = (160, 128)):
+        self.framerate = framerate
+        self.resolution = resolution
+
+        self.camera = PiCamera(framerate = self.framerate)
+        self.camera.resolution = self.resolution
+
+        time.sleep(2) #give camera time to setup and change resolution
+
+    def get_image(self):
+        img = np.empty((self.resolution[1]*self.resolution[0]*3,), dtype = np.uint8)
+        self.camera.capture(img, "bgr", use_video_port=True)
+        img = img.reshape((self.resolution[1], self.resoltuion[0], 3))
+
+        return img
---
+++ on_robot/test_camera_motors.py
@@ -0,0 +1,23 @@
+import penguinPi as ppi
+import time
+
+if __name__ == "__main__":
+    print("test left motor")
+    ppi.set_velocity(10,0)
+    time.sleep(2)
+    print("test right motor")
+    ppi.set_velocity(0,10)
+    time.sleep(2)
+    print("stop")
+    ppi.set_velocity(0,0)
+
+    print("initialise camera")
+    camera = ppi.CameraPiBot()
+    print("capture image")
+    image = camera.get_image()
+    print("image size %d by %d" % (image.shape[0],image.shape[1]))
+    start = time.time()
+    for i in range(100):
+        image = camera.get_image()
+    end = time.time()
+    print("frame rate is %f.3 fps" % (100/(end-start)))
