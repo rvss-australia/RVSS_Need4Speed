fd1b6f30e149434d9aedc47ff3e36b661c40ab25
Dimity Miller <41029982+dimitymiller@users.noreply.github.com>
2024-01-16 12:09:13 +1000

    Update collect.py

--- scripts/collect.py
+++ scripts/collect.py
@@ -5,13 +5,19 @@ import os
 import cv2
 import numpy as np
 from pynput import keyboard
+import argparse
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.abspath(os.path.join(script_path, "../PenguinPi-robot/software/python/client/")))
 from pibot_client import PiBot
 
+parser = argparse.ArgumentParser(description='PiBot client')
+parser.add_argument('--ip', type=str, default='localhost', help='IP address of PiBot')
+args = parser.parse_args()
+
+bot = PiBot(ip=args.ip)
 # stop the robot
-bot = PiBot('172.20.10.2')
+
 bot.setVelocity(0, 0)
 
 #countdown before beginning
