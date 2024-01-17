1f74adf7bf6366547e6788134d533e5194fdbc13
Dimity Miller <41029982+dimitymiller@users.noreply.github.com>
2024-01-17 15:10:35 +1000

    updated deploy to take ip argument

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -8,12 +8,21 @@ import sys
 import numpy as np
 import torch
 import torch.nn as nn
-
+import argparse
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
 script_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.abspath(os.path.join(script_path, "../PenguinPi-robot/software/python/client/")))
 from pibot_client import PiBot
 
-bot = PiBot('192.168.50.5')
+
+parser = argparse.ArgumentParser(description='PiBot client')
+parser.add_argument('--ip', type=str, default='localhost', help='IP address of PiBot')
+args = parser.parse_args()
+
+bot = PiBot(ip=args.ip)
+
 # stop the robot 
 bot.setVelocity(0, 0)
 
@@ -36,20 +45,22 @@ try:
     angle = 0
     while True:
         # get an image from the the robot
-        img = bot.getImage()
+        im = bot.getImage()
 
-        #TO DO: apply any image transformations
+        #TO DO: apply any necessary image transforms
 
-        #TO DO: pass image through network to get a prediction for the steering angle
-        
-        angle = np.clip(angle, -0.5, 0.5)
-        Kd = 30 #base wheel speeds, increase to go faster, decrease to go slower
-        Ka = 30 #how fast to turn when given an angle
+        #TO DO: pass image through network get a prediction
+
+        #TO DO: convert prediction into a meaningful steering angle
+        angle = 0
+
+        Kd = 20 #base wheel speeds, increase to go faster, decrease to go slower
+        Ka = 20 #how fast to turn when given an angle
         left  = int(Kd + Ka*angle)
         right = int(Kd - Ka*angle)
-        
+            
         bot.setVelocity(left, right)
-        
+            
         
 except KeyboardInterrupt:    
     bot.setVelocity(0, 0)
