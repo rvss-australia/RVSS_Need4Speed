2e0555553ae5e0e2ac7762fb46b31b72df53c986
Dimity Miller <41029982+dimitymiller@users.noreply.github.com>
2025-01-29 16:37:23 +1000

    Update collect.py

--- scripts/collect.py
+++ scripts/collect.py
@@ -78,8 +78,8 @@ try:
         img = bot.getImage()
         
         angle = np.clip(angle, -0.5, 0.5)
-        Kd = 20  # Base wheel speeds
-        Ka = 20  # Turn speed
+        Kd = 15  # Base wheel speeds
+        Ka = 15  # Turn speed
         left  = int(Kd + Ka*angle)
         right = int(Kd - Ka*angle)
         
