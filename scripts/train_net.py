7b93f4a5ea3c38ff4d2715333ec8f6fc4685c9c7
Dimity Miller <d24.miller@qut.edu.au>
2025-01-30 10:07:55 +1000

    pytorch classifier tutorial as starting point for training

--- scripts/train_net.py
+++ scripts/train_net.py
@@ -1,17 +1,26 @@
 import torch
 import torchvision.transforms as transforms
-import os
 from torch.utils.data import DataLoader
-import numpy as np
 import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import os
+import numpy as np
+import sklearn.metrics as metrics
+
 import matplotlib.pyplot as plt
 
 from steerDS import SteerDataSet
 
-transform = transforms.Compose(
-[transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+#######################################################################################################################################
+####     This tutorial is adapted from the PyTorch "Train a Classifier" tutorial                                                   ####
+####     Please review here if you get stuck: https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html                   ####
+#######################################################################################################################################
 
+
+#Helper function for visualising images in our dataset
 def imshow(img):
     img = img / 2 + 0.5 #unnormalize
     npimg = img.numpy()
@@ -20,30 +29,181 @@ def imshow(img):
     plt.imshow(rgbimg)
     plt.show()
 
-#### Setup the training dataset
+#######################################################################################################################################
+####     SETTING UP THE DATASET                                                                                                    ####
+#######################################################################################################################################
+
+#transformations for raw images before going to CNN
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Resize((40, 60)),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                                ])
+
 script_path = os.path.dirname(os.path.realpath(__file__))
+
+###################
+## Train dataset ##
+###################
 
 train_ds = SteerDataSet(os.path.join(script_path, '..', 'data', 'train_starter'), '.jpg', transform)
 print("The train dataset contains %d images " % len(train_ds))
 
+#data loader nicely batches images for the training process and shuffles (if desired)
 trainloader = DataLoader(train_ds,batch_size=8,shuffle=True)
 all_y = []
 for S in trainloader:
     im, y = S    
-        
     all_y += y.tolist()
 
-print(f'Input shape: {im.shape}')
+print(f'Input to network shape: {im.shape}')
 
+#visualise the distribution of GT labels
 all_lbls, all_counts = np.unique(all_y, return_counts = True)
-plt.bar(all_lbls, all_counts, width = 0.05)
+plt.bar(all_lbls, all_counts, width = (all_lbls[1]-all_lbls[0])/2)
 plt.xlabel('Labels')
 plt.ylabel('Counts')
 plt.xticks(all_lbls)
+plt.title('Training Dataset')
 plt.show()
 
+# visualise some images and print labels -- check these seem reasonable
 example_ims, example_lbls = next(iter(trainloader))
-# show images and print labels
 print(' '.join(f'{example_lbls[j]}' for j in range(len(example_lbls))))
 imshow(torchvision.utils.make_grid(example_ims))
 
+
+########################
+## Validation dataset ##
+########################
+
+val_ds = SteerDataSet(os.path.join(script_path, '..', 'data', 'val_starter'), '.jpg', transform)
+print("The train dataset contains %d images " % len(val_ds))
+
+#data loader nicely batches images for the training process and shuffles (if desired)
+valloader = DataLoader(val_ds,batch_size=1)
+all_y = []
+for S in valloader:
+    im, y = S    
+    all_y += y.tolist()
+
+print(f'Input to network shape: {im.shape}')
+
+#visualise the distribution of GT labels
+all_lbls, all_counts = np.unique(all_y, return_counts = True)
+plt.bar(all_lbls, all_counts, width = (all_lbls[1]-all_lbls[0])/2)
+plt.xlabel('Labels')
+plt.ylabel('Counts')
+plt.xticks(all_lbls)
+plt.title('Validation Dataset')
+plt.show()
+
+#######################################################################################################################################
+####     INITIALISE OUR NETWORK                                                                                                    ####
+#######################################################################################################################################
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.fc1 = nn.Linear(1344, 5)
+
+        self.relu = nn.ReLU()
+
+
+    def forward(self, x):
+        #extract features with convolutional layers
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        
+        #linear layer for classification
+        x = self.fc1(x)
+       
+        return x
+    
+
+net = Net()
+
+#######################################################################################################################################
+####     INITIALISE OUR LOSS FUNCTION AND OPTIMISER                                                                                ####
+#######################################################################################################################################
+
+#for classification tasks
+criterion = nn.CrossEntropyLoss()
+#You could use also ADAM
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+#######################################################################################################################################
+####     TRAINING LOOP                                                                                                             ####
+#######################################################################################################################################
+
+for epoch in range(10):  # loop over the dataset multiple times
+
+    epoch_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        epoch_loss += loss.item()
+
+    print(f'Epoch {epoch + 1} loss: {epoch_loss / len(trainloader)}')
+
+print('Finished Training')
+torch.save(net.state_dict(), 'steer_net.pth')
+
+
+#######################################################################################################################################
+####     PERFORMANCE EVALUATION                                                                                                    ####
+#######################################################################################################################################
+
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in valloader:
+        images, labels = data
+        # calculate outputs by running images through the network
+        outputs = net(images)
+        
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy of the network on the {total} test images: {100 * correct // total} %')
+
+# prepare to count predictions for each class
+correct_pred = {classname: 0 for classname in val_ds.class_labels}
+total_pred = {classname: 0 for classname in val_ds.class_labels}
+
+# again no gradients needed
+with torch.no_grad():
+    for data in valloader:
+        images, labels = data
+        outputs = net(images)
+        _, predictions = torch.max(outputs, 1)
+        # collect the correct predictions for each class
+        for label, prediction in zip(labels, predictions):
+            if label == prediction:
+                correct_pred[val_ds.class_labels[label]] += 1
+            total_pred[val_ds.class_labels[label]] += 1
+
+# print accuracy for each class
+for classname, correct_count in correct_pred.items():
+    accuracy = 100 * float(correct_count) / total_pred[classname]
+    print(f'Accuracy for class: {classname:5s} is {accuracy:.1f}%')
